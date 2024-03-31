87f2c42342ce3094d18e769d4aa8b06dea872dbb
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-03-31 17:18:28 +0200

    Add Winston to create a log system

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('./config/logger');
 
 const app = express();
 
@@ -6,13 +7,19 @@ app.get('/', (req, res, next) => res.send('Welcome to the API'));
 
 // No route fount handler
 app.use((req, res, next) => {
-  res.status(404);
-  res.json({ message: 'Error. Route not fount' });
+  const message = 'Route not fount';
+  const statusCode = 404;
+  logger.warn(message);
+  res.status(statusCode);
+  res.json({
+    message,
+  });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
+  logger.error(message);
   res.status(statusCode);
   res.json({
     message,
