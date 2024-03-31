3e7bf63b915a2956008db76c73f671681bde8fd7
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-03-31 17:09:16 +0200

    Add middleware for not fount routes and errors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res, next) => res.send('Welcome to the API'));
+
+// No route fount handler
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ message: 'Error. Route not fount' });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
 
 module.exports = app;
