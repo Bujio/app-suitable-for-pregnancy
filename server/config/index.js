1b4d63ffbc593b85a00270f84025373b4ffcf984
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-03-31 16:41:55 +0200

    Add config file

---
+++ server/config/index.js
@@ -0,0 +1,5 @@
+const config = {
+  server: { port: 3000 },
+};
+
+module.exports = config;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 
-const PORT = 3000;
-
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = app;
